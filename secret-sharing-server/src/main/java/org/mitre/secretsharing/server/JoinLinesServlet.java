2ff9143141020f2cd28dbd633e4e6836b0839ac1
robin <robin.kirkman@gmail.com>
2015-08-28 21:05:31 -0400

    JoinLinesServlet

---
+++ secret-sharing-server/src/main/java/org/mitre/secretsharing/server/JoinLinesServlet.java
@@ -0,0 +1,41 @@
+package org.mitre.secretsharing.server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.mitre.secretsharing.Part;
+import org.mitre.secretsharing.Secrets;
+
+import com.fasterxml.jackson.core.Base64Variants;
+
+public class JoinLinesServlet extends HttpServlet {
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		try {
+			BufferedReader lines = new BufferedReader(new InputStreamReader(req.getInputStream(), Charset.forName("UTF-8")));
+			List<Part> parts = new ArrayList<>();
+			for(String line = lines.readLine(); line != null; line = lines.readLine()) {
+				if(line.trim().isEmpty())
+					continue;
+				parts.add(new Part(line));
+			}
+			byte[] secret = Secrets.join(parts.toArray(new Part[parts.size()]));
+			Writer writer = new OutputStreamWriter(resp.getOutputStream(), Charset.forName("UTF-8"));
+			writer.write(Base64Variants.MIME.encode(secret));
+			writer.flush();
+		} catch(Exception e) {
+		}
+		resp.getOutputStream().close();
+	}
+}
